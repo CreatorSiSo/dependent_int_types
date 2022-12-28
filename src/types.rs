0adf557182175d06e635f5bdf838f3e5b98115bd
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2022-12-28 14:52:09 +0100

    Implement addition for Aint.

--- src/types.rs
+++ src/types.rs
@@ -25,6 +25,10 @@ impl ArbitraryInt {
 		Self { data, length }
 	}
 
+	pub fn from_data(data: u128) -> Self {
+		Self { data, length: 0 }.resize()
+	}
+
 	pub fn resize(mut self) -> Self {
 		for (index, max) in uint_max_lut().iter().enumerate() {
 			if self.data <= *max {
@@ -36,40 +40,102 @@ impl ArbitraryInt {
 	}
 }
 
-#[test]
-fn test_shrink() {
-	let expect = [
-		(ArbitraryInt::new(1, 128), ArbitraryInt::new(1, 1)),
-		(ArbitraryInt::new(3, 128), ArbitraryInt::new(3, 2)),
-		(ArbitraryInt::new(7, 128), ArbitraryInt::new(7, 3)),
-		(ArbitraryInt::new(15, 128), ArbitraryInt::new(15, 4)),
-		(ArbitraryInt::new(31, 128), ArbitraryInt::new(31, 5)),
-		(ArbitraryInt::new(63, 128), ArbitraryInt::new(63, 6)),
-		(ArbitraryInt::new(127, 128), ArbitraryInt::new(127, 7)),
-		(ArbitraryInt::new(255, 128), ArbitraryInt::new(255, 8)),
-		(ArbitraryInt::new(511, 128), ArbitraryInt::new(511, 9)),
-		(ArbitraryInt::new(1023, 128), ArbitraryInt::new(1023, 10)),
-		(ArbitraryInt::new(2047, 128), ArbitraryInt::new(2047, 11)),
-		(ArbitraryInt::new(4095, 128), ArbitraryInt::new(4095, 12)),
-		(
-			ArbitraryInt::new(u16::MAX as u128, 128),
-			ArbitraryInt::new(u16::MAX as u128, 16),
-		),
-		(
-			ArbitraryInt::new(u32::MAX as u128, 128),
-			ArbitraryInt::new(u32::MAX as u128, 32),
-		),
-		(
-			ArbitraryInt::new(u64::MAX as u128, 128),
-			ArbitraryInt::new(u64::MAX as u128, 64),
-		),
-		(
-			ArbitraryInt::new(u128::MAX, 128),
-			ArbitraryInt::new(u128::MAX, 128),
-		),
-	];
+impl std::ops::Add for ArbitraryInt {
+	type Output = ArbitraryInt;
 
-	for (int, expect) in expect {
-		assert_eq!(int.resize(), expect)
+	fn add(mut self, rhs: Self) -> Self::Output {
+		self.data += rhs.data;
+		self.resize()
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+	type Aint = ArbitraryInt;
+
+	#[test]
+	fn aint_from_data() {
+		let expect = [
+			(Aint::from_data(1), Aint::new(1, 1)),
+			(Aint::from_data(3), Aint::new(3, 2)),
+			(Aint::from_data(7), Aint::new(7, 3)),
+			(Aint::from_data(15), Aint::new(15, 4)),
+			(Aint::from_data(31), Aint::new(31, 5)),
+			(Aint::from_data(63), Aint::new(63, 6)),
+			(Aint::from_data(127), Aint::new(127, 7)),
+			(Aint::from_data(255), Aint::new(255, 8)),
+			(Aint::from_data(511), Aint::new(511, 9)),
+			(Aint::from_data(1023), Aint::new(1023, 10)),
+			(Aint::from_data(2047), Aint::new(2047, 11)),
+			(Aint::from_data(4095), Aint::new(4095, 12)),
+			(
+				Aint::from_data(u16::MAX as u128),
+				Aint::new(u16::MAX as u128, 16),
+			),
+			(
+				Aint::from_data(u32::MAX as u128),
+				Aint::new(u32::MAX as u128, 32),
+			),
+			(
+				Aint::from_data(u64::MAX as u128),
+				Aint::new(u64::MAX as u128, 64),
+			),
+			(Aint::from_data(u128::MAX), Aint::new(u128::MAX, 128)),
+		];
+
+		for (int, expect) in expect {
+			assert_eq!(int, expect)
+		}
+	}
+
+	#[test]
+	fn resize_aint() {
+		let expect = [
+			(Aint::new(1, 128), Aint::new(1, 1)),
+			(Aint::new(3, 128), Aint::new(3, 2)),
+			(Aint::new(7, 128), Aint::new(7, 3)),
+			(Aint::new(15, 128), Aint::new(15, 4)),
+			(Aint::new(31, 128), Aint::new(31, 5)),
+			(Aint::new(63, 128), Aint::new(63, 6)),
+			(Aint::new(127, 128), Aint::new(127, 7)),
+			(Aint::new(255, 128), Aint::new(255, 8)),
+			(Aint::new(511, 128), Aint::new(511, 9)),
+			(Aint::new(1023, 128), Aint::new(1023, 10)),
+			(Aint::new(2047, 128), Aint::new(2047, 11)),
+			(Aint::new(4095, 128), Aint::new(4095, 12)),
+			(
+				Aint::new(u16::MAX as u128, 128),
+				Aint::new(u16::MAX as u128, 16),
+			),
+			(
+				Aint::new(u32::MAX as u128, 128),
+				Aint::new(u32::MAX as u128, 32),
+			),
+			(
+				Aint::new(u64::MAX as u128, 128),
+				Aint::new(u64::MAX as u128, 64),
+			),
+			(Aint::new(u128::MAX, 128), Aint::new(u128::MAX, 128)),
+		];
+
+		for (int, expect) in expect {
+			assert_eq!(int.resize(), expect)
+		}
+	}
+
+	#[test]
+	fn aint_add() {
+		let expect = [
+			(Aint::from_data(1) + Aint::from_data(1), Aint::new(2, 2)),
+			(
+				Aint::from_data(123) + Aint::from_data(132),
+				Aint::new(255, 8),
+			),
+		];
+
+		for (int, expect) in expect {
+			assert_eq!(int, expect)
+		}
 	}
 }
